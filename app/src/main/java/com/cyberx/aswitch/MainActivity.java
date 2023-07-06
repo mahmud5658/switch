b854d97ea60bde7fc1958b540abc1e6b62f581b1
Abdullah <almahmuda5@gmail.com>
2023-07-06 22:04:00 +0600

    switch

---
+++ app/src/main/java/com/cyberx/aswitch/MainActivity.java
@@ -0,0 +1,34 @@
+package com.cyberx.aswitch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Switch aSwitch;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        aSwitch = (Switch) findViewById(R.id.switch_id);
+
+        aSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    Toast.makeText(MainActivity.this, "Wifi is on", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Wifi is off", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
